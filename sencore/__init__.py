92db4e96f083f53839c80c46b91f1fb1c11648ea
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2021-01-25 15:16:03 +0800

    add sentence marker

--- sencore/__init__.py
+++ sencore/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from .parser import Parser
 from .vocab_parser import VocabParser
--- sencore/phrase_parser.py
+++ sencore/phrase_parser.py
@@ -3,6 +3,35 @@ import spacy
 #from spacy import displacy
 from phrase_recognizer import NounPhraseRecognizer, PrepPhraseRecognizer, VerbKnowledgeRecognizer
 
+def merge_ranges(ranges):
+  ordered = sorted(ranges, key=lambda x: x[0])
+  purified = []
+  index = 0
+  while index < len(ordered)-1:
+    first, second = ordered[index], ordered[index+1]
+    if first[1] > second[0]:
+      purified.append((first[0], max(first[1], second[1]), first[2]))
+      index = index + 2
+    else:
+      purified.append(first)
+      index = index + 1
+  if index == len(ordered)-1:
+    purified.append(ordered[index])
+  return purified
+
+def extend_ranges(ranges, maxlen):
+  ordered = merge_ranges(ranges)
+  start = 0
+  result = []
+  for e in ordered:
+    if start < e[0]:
+      result.append((start, e[0], "plain"))
+    result.append(e)
+    start = e[1]
+  if start < maxlen:
+    result.append((start, maxlen, "plain"))
+  return result
+
 class PhraseParser(Parser):
 
   pkgindices = {
@@ -31,11 +60,19 @@ class PhraseParser(Parser):
     passive_phrases = [pp.text for pp in doc._.passive_phrases]
     verb_phrases = doc._.verb_phrases
 
+    noun_phrases_ranges = [(np.start, np.end, "noun_phrases") for np in doc._.noun_phrases]
+    prep_phrases_ranges = [(pp.start, pp.end, "prep_phrases") for pp in doc._.prep_phrases]
+    verbs_ranges = [(v.i, v.i+1, "verbs") for v in doc._.verbs]
+
+    doc_mark = extend_ranges(noun_phrases_ranges+prep_phrases_ranges+verbs_ranges, len(doc))
+    markers = [(doc[d[0]: d[1]].text, d[2]) for d in doc_mark]
+
     return {
       "noun_phrases": noun_phrases,
       "prep_phrases": prep_phrases,
       "verbs": verbs,
       "passive_phrases": passive_phrases,
-      "verb_phrases": verb_phrases
+      "verb_phrases": verb_phrases,
+      "markers": markers
     }
 
